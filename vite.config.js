7b20840e47c236bbcc5d2e808c91241b1d3ff91b
Manoj <rayimanoj8@gmail.com>
2025-01-27 00:47:36 +0530

    added deployment configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/chat-app-frontend",
   plugins: [react()],
   resolve: {
     alias: {
